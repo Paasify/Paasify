bfde429a69d4646b0b5df8d1c41882e82cb06150
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-08-19 17:54:30 +0000

    Update users route

--- apps/api/routes/users.js
+++ apps/api/routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const Authentication = require('../utils/authentication')
+var express = require('express')
+var router = express.Router()
+const passport = require('passport')
+// const handlers = require('../utils/handlers');
+
+const requireAuth = passport.authenticate('jwt', {
+  session: false,
+})
+const requireSignIn = passport.authenticate('local', {
+  session: false,
+})
+
+/* GET Default Page */
+router.get('/', function (req, res) {
+  res.status(200).send({ status: 200, message: 'Auth API is working' })
+})
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/validate', requireAuth, function (req, res) {
+  console.log('/validate route ran')
+  res.send({
+    user: req.user.email,
+  })
+})
 
-module.exports = router;
+// Login user
+router.post('/login', requireSignIn, Authentication.login)
+
+// Register user
+router.post('/signup', Authentication.signup)
+
+module.exports = router
