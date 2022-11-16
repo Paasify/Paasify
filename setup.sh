d56ca61819e7b02f1a18dddf906b581de963105b
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-11-16 11:27:38 +0000

    Backup Stuff

--- setup.sh
+++ setup.sh
@@ -61,12 +61,33 @@ while true; do
     esac
 done
 
+# Check if docker is installed. If not, install it.
+if ! [ -x "$(command -v docker)" ]; then
+    while true; do
+        read -p "Docker Engine not found, should I install it automatically? [Yy/Nn] " yn
+        case $yn in
+        [Yy]*)
+            echo "Installing Docker."
+            sh -c "$(curl --silent -fsSL https://get.docker.com)"
+            restartDocker
+            break
+            ;;
+        [Nn]*)
+            echo "Please install docker manually and update it to the latest, but at least to $DOCKER_VERSION"
+            exit 0
+            ;;
+        *) echo "Please answer Y or N." ;;
+        esac
+    done
+fi
+
+
 # Check if pack is installed. If not, install it.
 if [ -x "$(command -v pack)" ]; then
     echo "pack is already installed. Please uninstall it and run this script again."
     exit 1
 else
-    (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)\
+    (curl -sSL "https://github.com/buildpacks/pack/releases/download/${PACK_VERSION}/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)\
     && echo "pack installed successfully."\
     || echo "pack installation failed."
 fi
@@ -88,10 +109,19 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
-# Check nodejs, npm and yarn
+# Check nodejs and yarn
 if [ -x "$(command -v node)" ]; then
     echo "nodejs is already installed. Please uninstall it and run this script again."
     exit 1
+elif [ -x "$(command -v nvm)" ]; then
+    echo "Using existing installation of nvm" && \
+    echo "Installing nodejs $NODE_VERSION" && \
+    nvm install $NODE_VERSION && \
+    nvm use $NODE_VERSION && \
+    echo "Installing yarn $YARN_VERSION" && \
+    npm install -g yarn@$YARN_VERSION && \
+    echo "nodejs and yarn installed successfully."\
+    || echo "nodejs and yarn installation failed."
 else
     echo "Installing nodejs and yarn..."
     (curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash)\
@@ -119,14 +149,105 @@ if [ -x "$(command -v traefik)" ]; then
     echo "Traefik is already installed. Please uninstall it and run this script again."
     exit 1
 else
-    echo "Installing Traefik..."
+    echo "Installing Traefik..." && \
+    echo "Create a Wildcard DNS record for your domain (Ex: * example.com 192.168.1.1)" && \
+    # read -p "Enter the wildcard domain/subdomain: " DOMAIN && \
+    while true; do
+        read -p "Enter the wildcard domain/subdomain: " DOMAIN
+        case $DOMAIN in
+            [Yy]* ) make install; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
     cd ~/traefik && \
     mkdir -p data/configurations && \
-    touch data/traefik.yml && \
-    touch data/acme.json && \
-    touch data/configurations/dynamic.yml && \
-    chmod 600 data/acme.json
-    
+    # touch data/traefik.yml && \
+    # touch data/acme.json && \
+    # touch data/configurations/dynamic.yml && \
+    # chmod 600 data/acme.json && \
+    echo -e "version: '3.7'
+
+services:
+  traefik:
+    image: traefik:v2.7
+    container_name: traefik
+    restart: always
+    security_opt:
+      - no-new-privileges:true
+    ports:
+      - 80:80
+      - 443:443
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      - ./data/traefik.yml:/traefik.yml:ro
+      - ./data/acme.json:/acme.json
+      - ./data/configurations:/configurations
+    networks:
+      - proxy
+    labels:
+      - "traefik.enable=true"
+      - "traefik.docker.network=proxy"
+      - "traefik.http.routers.traefik-secure.entrypoints=websecure"
+      - "traefik.http.routers.traefik-secure.rule=Host(`${TRAEFIK_DOMAIN}`)"
+      - "traefik.http.routers.traefik-secure.middlewares=user-auth@file"
+      - "traefik.http.routers.traefik-secure.service=api@internal"
+
+networks:
+  proxy:
+    external: true
+" > docker-compose.yml
+
+# Editing traefik configuration file
+echo -e "api:
+  dashboard: true
+
+entryPoints:
+  web:
+    address: :80
+    http:
+      redirections:
+        entryPoint:
+          to: websecure
+
+  websecure:
+    address: :443
+    http:
+      middlewares:
+        - secureHeaders@file
+        - nofloc@file
+      tls:
+        certResolver: letsencrypt
+
+pilot:
+  dashboard: false
+
+providers:
+  docker:
+    endpoint: "unix:///var/run/docker.sock"
+    exposedByDefault: false
+  file:
+    filename: /configurations/dynamic.yml
+
+certificatesResolvers:
+  letsencrypt:
+    acme:
+      email: admin@yourdomain
+      storage: acme.json
+      keyType: EC384
+      httpChallenge:
+        entryPoint: web
+
+  buypass:
+    acme:
+      email: admin@yourdomain
+      storage: acme.json
+      caServer: https://api.buypass.com/acme/directory
+      keyType: EC256
+      httpChallenge:
+        entryPoint: web
+"
 
 # Run paasify with pm2
 echo "Starting Paasify..."
