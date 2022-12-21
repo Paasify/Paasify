02bc1e7fd72f23b2b7198651c53c835b1c36bbe6
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-12-21 18:57:02 +0000

    Add new route for getting user registration status Improve formatting

--- apps/api/routes/users.js
+++ apps/api/routes/users.js
@@ -1,17 +1,25 @@
-var express = require('express');
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+var express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 var { authJwt } = require("../middleware");
 var router = express.Router();
 
 /* GET test access to everyone */
-router.get('/', function(req, res, next) {
-  res.send('Access to everyone');
+router.get("/", function (req, res, next) {
+  res.send("Access to everyone");
 });
 
 /* GET test access to admin only */
-router.get('/dashboard', [authJwt.verifyToken], function(req, res, next) {
-  res.send('only if the jwt is verified');
-})
+router.get("/dashboard", [authJwt.verifyToken], function (req, res, next) {
+  res.send("only if the jwt is verified");
+});
+
+/* GET User registered */
+router.get("/registered", async function (req, res) {
+  const count = await prisma.user.count();
+  if (count !== 0) {
+    res.send({ registered: true, message: "User registered" });
+  }
+});
 
 module.exports = router;
