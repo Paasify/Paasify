4b88412a0a692f72f66c26d128d9b4d3a13054a1
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-12-21 19:00:15 +0000

    Changed a lot of stuff but the main one was the addition of React-Auth-Kit

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -1,33 +1,30 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, redirect } from 'react-router-dom';
-import Login from './pages/Login';
-import RedirectToLogin from './components/RedirectLogin';
-import { history } from './helpers/history';
-import { clearMessage } from './actions/message';
-import EventBus from "./common/EventBus";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "react-auth-kit";
+import { RequireAuth } from "react-auth-kit";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import RedirectLogin from "./components/RedirectLogin";
 
 function App() {
-  history.listen(() => {
-    clearMessage();
-  });
-  
-  useEffect(() => {
-    EventBus.on("logout", () => {
-      logout();
-    });
-    
-    return () => {
-      EventBus.remove("logout");
-    };
-  })
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<RedirectToLogin />} />
-        <Route exact path="/login" element={<Login />} />
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
-      </Routes>
-    </Router>
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<RedirectLogin />} />
+          <Route exact path="/dashboard" element={<RequireAuth loginPath="/login"><Dashboard /></RequireAuth>} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
