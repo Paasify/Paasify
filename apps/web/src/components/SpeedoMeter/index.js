da044eaaccc50afae335d57ce85c6839ef39e6f0
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-12-25 11:36:51 +0000

    Add speedometer for displaying cpu and memory stat

---
+++ apps/web/src/components/SpeedoMeter/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+export default function SpeedoMeter({ percent, current, total, type}) {
+    return (
+        <SpeedoMeterWrapper>
+            <h1>{type}</h1>
+            <Percent>{percent}<span>%</span></Percent>
+            {type === "CPU" ? (<h1>{total} cores</h1>) : (<h1>{current} of {total} MB (~{Math.round(total*100/1024)/100} GB)</h1>)}
+        </SpeedoMeterWrapper>
+    )
+}
+
+const SpeedoMeterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    border: 1px solid black;
+    padding: 2rem;
+    h1 {
+        font-size: 1.5rem;
+        text-decoration: underline;
+    }
+`
+
+const Percent = styled.div`
+    font-size: 8rem;
+    span {
+        position: absolute;
+        font-size: 2rem;
+    }
+`
