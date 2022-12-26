c9dde65fe61ca4b6b1298fbf87eb135658004e6f
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2022-12-26 01:43:48 +0000

    Add support for listing containers

--- apps/web/src/pages/Dashboard/index.js
+++ apps/web/src/pages/Dashboard/index.js
@@ -27,17 +27,48 @@ export default function Dashboard() {
             socket.off("server usage")
         }
     }, [])
+    console.log(serverUsage)
     return (
         <DashboardWrapper>
             <Navbar />
             <SpeedoMeterWrapper>
                 <SpeedoMeterWidthWrapper>
-                    {serverUsage ? (<SpeedoMeter percent={serverUsage.cpu[0]} total={serverUsage.processorCount} type="CPU" arch={serverUsage.cpuArch}>yo</SpeedoMeter>) : (<h1>Loading...</h1>)}
+                    {serverUsage ? (<SpeedoMeter percent={serverUsage.cpu[0]} total={serverUsage.processorCount} type="CPU" arch={serverUsage.cpuArch}>yo</SpeedoMeter>) : (<h1 style={{border: '1px solid black'}}>Loading...</h1>)}
                 </SpeedoMeterWidthWrapper>
                 <SpeedoMeterWidthWrapper>
-                    {serverUsage ? (<SpeedoMeter percent={Math.round((serverUsage.mem.totalMem - serverUsage.mem.freeMem)*10000/serverUsage.mem.totalMem)/100} current={Math.round(serverUsage.mem.totalMem - serverUsage.mem.freeMem)} total={Math.round(serverUsage.mem.totalMem)} type="Memory">yo</SpeedoMeter>) : (<h1>Loading...</h1>)}
+                    {serverUsage ? (<SpeedoMeter percent={Math.round((serverUsage.mem.totalMem - serverUsage.mem.freeMem)*10000/serverUsage.mem.totalMem)/100} current={Math.round(serverUsage.mem.totalMem - serverUsage.mem.freeMem)} total={Math.round(serverUsage.mem.totalMem)} type="Memory">yo</SpeedoMeter>) : (<h1 style={{border: '1px solid black'}}>Loading...</h1>)}
                 </SpeedoMeterWidthWrapper>
             </SpeedoMeterWrapper>
+            <ContainerWrapper>
+                <DockerContainers>
+                    <DockerTitle>Containers (Running)</DockerTitle>
+                    {serverUsage ? (
+                    <table>
+                        <tr>
+                            <th>CONTAINER ID</th>
+                            <th>IMAGE</th>
+                            <th>COMMAND</th>
+                            <th>CREATED</th>
+                            <th>STATUS</th>
+                        </tr>
+                        {serverUsage.containersList.map((item, i) => {
+                            return(
+                                <>
+                                    <tr key={i}>
+                                        <td>{item.Id.substring(0,12)}</td>
+                                        <td>{item.Image}</td>
+                                        <td>{item.Command}</td>
+                                        <td>{item.Created}</td>
+                                        <td>{item.Status}</td>
+                                    </tr>
+                                </>
+                            )
+                        })}
+                    </table>
+                    ) : <h1>Loading...</h1>
+                    }
+                </DockerContainers>
+            </ContainerWrapper>
         </DashboardWrapper>
     )
 }
@@ -57,5 +88,41 @@ const SpeedoMeterWrapper = styled.div`
 `
 
 const SpeedoMeterWidthWrapper = styled.div`
-width: 40vw;
+    width: 40vw;
+`
+
+const ContainerWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+    
+const DockerContainers = styled.div` 
+    width: calc(80vw - 2px);
+    border: 1px solid;
+    display: flex;
+    gap: 1rem;
+    flex-direction: column;
+    align-items: center;
+    table {
+        width: 90%;
+        margin-bottom: 2rem;
+        th {
+            font-weight: 600;
+        }
+        td, th {
+            border: 1px solid #dddddd;
+            padding: 8px;
+            text-align: left;
+        }
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    }
+`
+
+const DockerTitle = styled.div`
+    margin-top: 2rem;
+    font-size: 1.5rem;
+    text-decoration: underline;
 `
