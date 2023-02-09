9ff8073c4a03a8e4bf36b0be4af093fb4d1d940c
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-09 21:29:31 +0000

    Add comment

--- apps/api/middleware/authJwt.js
+++ apps/api/middleware/authJwt.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 
 verifyToken = (req, res, next) => {
     let token = req.cookies._auth;
-    console.log(token)
+    // console.log(token)
     if (!token) {
         return res.status(401).send({
             message: "Unauthorized access."
@@ -12,7 +12,7 @@ verifyToken = (req, res, next) => {
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        console.log('decoded', decoded)
+        // console.log('decoded', decoded)
         if (err) {
             return res.status(401).send({
                 message: "Token is not valid!"
@@ -21,8 +21,6 @@ verifyToken = (req, res, next) => {
         req.userId = decoded.id;
         next();
     })
-    // console.log("Token not valid!")
-    // return res.status(401).send({ message: "Wrong Token!" });
 }
 
 module.exports = { verifyToken }
