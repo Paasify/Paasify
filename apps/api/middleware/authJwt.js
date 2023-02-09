c603a751e5ef428ab2a34633a9b9e3f28a05dbc7
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-09 21:09:17 +0000

    Comment Debug Lines (console.log is convenient to use :sigh:)

--- apps/api/middleware/authJwt.js
+++ apps/api/middleware/authJwt.js
@@ -4,6 +4,7 @@ const prisma = new PrismaClient()
 
 verifyToken = (req, res, next) => {
     let token = req.cookies._auth;
+    console.log(token)
     if (!token) {
         return res.status(403).send({
             message: "No token provided!"
@@ -11,6 +12,7 @@ verifyToken = (req, res, next) => {
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        console.log('decoded', decoded)
         if (err) {
             return res.status(401).send({
                 message: "Unauthorized!"
@@ -19,6 +21,7 @@ verifyToken = (req, res, next) => {
         req.userId = decoded.id;
         next();
     })
+    console.log("Token not valid!")
     res.status(401).send({ message: "Wrong Token!" });
 }
 
